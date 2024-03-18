87ba7c90fee08b34ae2c527d58761b5187589b18
KageRyo <k6.developer@gmail.com>
2024-03-18 20:34:23 +0800

    update: Django settings.py.

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -31,12 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',        # 管理者後台
+    'django.contrib.auth',         # 認證授權管理
+    'django.contrib.contenttypes', # 內容類型管理
+    'django.contrib.sessions',     # session 管理
+    'django.contrib.messages',     # 訊息管理
+    'django.contrib.staticfiles',  # 靜態檔案管理
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -53,15 +54,15 @@ ROOT_URLCONF = "myweb.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'zh-Hant'   # 正體中文
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Taipei'   # 亞州/台北時區
 
 USE_I18N = True
 
@@ -115,8 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
